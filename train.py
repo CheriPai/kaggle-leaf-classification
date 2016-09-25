0e223b5173f8f45ad5eac03d408b0bba7f2a911f
dat <datdo@rocketmail.com>
2016-09-25 12:32:51 -0700

    Tune RF hyperparams and serialize to disk

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn import cross_validation
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
 
 
 leaves = pd.read_csv("data/train.csv")
@@ -11,7 +12,9 @@ y = leaves.species.values
 X_train, X_val, y_train, y_val = cross_validation.train_test_split(
     X, y, test_size=0.2, random_state=0)
 
-clf = RandomForestClassifier()
+clf = RandomForestClassifier(n_estimators=100, max_features="log2", n_jobs=-1)
 clf.fit(X_train, y_train)
 
 print("Accuracy: %.4f" % clf.score(X_val, y_val))
+print("Saving to data/classifier.pkl")
+joblib.dump(clf, "data/classifier.pkl")
