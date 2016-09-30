2c067690c4d78c2195b268798a49575ea738359a
cheripai <datdo@rocketmail.com>
2016-09-30 13:00:35 -0700

    Add id column to predictions

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.externals import joblib
 
@@ -5,5 +6,8 @@ from sklearn.externals import joblib
 leaves = pd.read_csv("data/test.csv")
 X = leaves.drop(["id"], axis=1).values
 
+labels = pd.read_csv("data/sample_submission.csv").columns.values
+
 clf = joblib.load("data/classifier.pkl")
 predictions = clf.predict_proba(X)
+predictions = np.concatenate((np.array([leaves.id]).T, predictions), axis=1)
