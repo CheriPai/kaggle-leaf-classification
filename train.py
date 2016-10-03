36b2e1d0297f6534d43f3a6cb39f34edf73745ef
dat <datdo@rocketmail.com>
2016-10-02 18:45:28 -0700

    Use sss and display log loss

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn import cross_validation, preprocessing
+from sklearn.cross_validation import StratifiedShuffleSplit
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
+from sklearn.metrics import accuracy_score, log_loss
 
 
 leaves = pd.read_csv("data/train.csv")
@@ -13,14 +15,20 @@ le = preprocessing.LabelEncoder()
 le.fit(labels)
 
 X = leaves.drop(["species"], axis=1).values
-y = leaves.species.values
+y = leaves.species
 y = le.transform(y)
-X_train, X_val, y_train, y_val = cross_validation.train_test_split(
-    X, y, test_size=0.2, random_state=0)
 
-clf = RandomForestClassifier(n_estimators=100, max_features="log2", n_jobs=-1)
+sss = StratifiedShuffleSplit(y, 10, test_size=0.2, random_state=23)
+for train_index, test_index in sss:
+    X_train, X_val = X[train_index], X[test_index]
+    y_train, y_val = y[train_index], y[test_index]
+
+clf = RandomForestClassifier(n_estimators=200, max_features="log2", n_jobs=-1)
 clf.fit(X_train, y_train)
 
-print("Accuracy: %.4f" % clf.score(X_val, y_val))
+predictions = clf.predict(X_val)
+print("Accuracy: %.4f" % accuracy_score(y_val, predictions))
+predictions = clf.predict_proba(X_val)
+print("Log loss: %.4f" % log_loss(y_val, predictions))
 print("Saving classifier to data/classifier.pkl")
 joblib.dump(clf, "data/classifier.pkl")
